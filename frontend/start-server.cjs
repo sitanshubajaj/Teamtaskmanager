4dbe14c919727528762141f9c8cfcf76b19d60b0
Your Name <your-email@example.com>
2026-05-03 19:18:02 +0530

    fix: force serve to listen on 0.0.0.0

--- frontend/start-server.cjs
+++ frontend/start-server.cjs
@@ -1,9 +1,9 @@
 const { spawn } = require('child_process');
 
 const port = process.env.PORT || 3000;
-console.log(`Starting server on port ${port}...`);
+console.log(`Starting server on port ${port} and interface 0.0.0.0...`);
 
-const serve = spawn('npx', ['serve', '-s', 'dist', '-l', port], {
+const serve = spawn('npx', ['serve', '-s', 'dist', '-l', `tcp://0.0.0.0:${port}`], {
   stdio: 'inherit',
   shell: true
 });
